0f7e158a1192f7af3ea558c465b33be08ed64702
Oscar Leal <oscarleal234@gmail.com>
2024-10-02 22:29:23 -0500

    working on Sign In page

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -28,26 +28,21 @@ const NavBar = () => {
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
           <Link color="foreground" href="#">
-            Features
+            DashBoard
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
           <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
+            Applications
           </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Link href="/login">Login</Link>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
+          <Button as={Link} color="primary" href="/signup" variant="flat">
             Sign Up
           </Button>
         </NavbarItem>
---
+++ app/signup/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Image from 'next/image';
+import InputB from './Input';
+
+import CatLaptop from '../../public/CatLaptop.png';
+const SignUpPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <form className="flex flex-col justify-center items-center gap-2 bg-white rounded-md ">
+        <Image
+          alt="CatPic"
+          className=""
+          src={CatLaptop}
+          height={200}
+          width={200}
+        />
+        <div className="flex justify-center items-start">
+          <InputB />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpPage;
