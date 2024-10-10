510cf26df04956e4a24b6248ffcc614083fb5990
Oscar Leal <oscarleal234@gmail.com>
2024-10-10 15:13:25 -0500

    adjusting sidebar

--- app/sidebar.tsx
+++ app/sidebar.tsx
@@ -11,7 +11,8 @@ import {
 } from '@nextui-org/react';
 import Logo from '../public/logo-no-background.png';
 import { useSession } from 'next-auth/react';
-
+import { FaClipboard } from 'react-icons/fa';
+import { CiCircleList } from 'react-icons/ci';
 interface Logo {
   src: string;
 }
@@ -23,19 +24,27 @@ const SideBar = () => {
   }
 
   return (
-    <Navbar className="bg-[#c1c8e4] fixed top-0 left-0 h-full w-[120px] flex flex-col">
-      <NavbarContent className="flex flex-col justify-between ">
-        <NavbarItem isActive>
-          <Link href="/" className="hover:underline" aria-current="page">
-            Dashboard
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="/settings" className="hover:underline">
-            Settings
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
+    <Navbar className="bg-[#001F3F] fixed top-0 left-0 h-full w-[120px] flex flex-col">
+      {status === 'authenticated' ? (
+        <>
+          <NavbarContent className="flex flex-col justify-around items-center">
+            <NavbarItem isActive>
+              <Link href="/" className="hover:underline" aria-current="page">
+                <FaClipboard className="text-indigo-300 text-5xl" />
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link href="/settings" className="hover:underline">
+                <CiCircleList className="text-indigo-300 text-5xl" />
+              </Link>
+            </NavbarItem>
+          </NavbarContent>
+        </>
+      ) : (
+        <NavbarContent justify="end">
+          <NavbarBrand></NavbarBrand>
+        </NavbarContent>
+      )}
     </Navbar>
   );
 };
