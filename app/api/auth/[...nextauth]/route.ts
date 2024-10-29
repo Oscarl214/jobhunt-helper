a5097bb6503a11f307f8733de46f990f7f3f3bb0
Oscar Leal <oscarleal234@gmail.com>
2024-10-28 20:17:42 -0500

    removed un used files from auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,5 @@
 import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '../../../../lib/prisma';
+
 import { authOptions } from '@/app/lib/authoptions';
 
 const handler = NextAuth(authOptions);
